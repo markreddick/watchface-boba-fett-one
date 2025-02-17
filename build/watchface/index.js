221843e496f0f5979f37d821c42f8f2abb7995a6
Mark Reddick <mark@mersjr.com>
2025-02-17 06:51:33 -0600

    removed shadows from small text and updated name and preview image

--- build/watchface/index.js
+++ build/watchface/index.js
@@ -2,15 +2,12 @@ import * as view from "./view.js";
 
 WatchFace({
   onInit() {
-    console.log('index page.js on init invoke')
   },
 
   build() {
-    console.log('index page.js on build invoke')
     view.buildView();
   },
 
   onDestroy() {
-    console.log('index page.js on destroy invoke')
   },
 })
