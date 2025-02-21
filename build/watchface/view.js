f2974e0b28e219cd357b1c871e6c892e1d3981fd
Mark Reddick <mark@mersjr.com>
2025-02-21 08:37:55 -0600

    increased size of dow and day subdial fonts and reordered so subdial hands appear below main time hands

--- build/watchface/view.js
+++ build/watchface/view.js
@@ -1,14 +1,9 @@
 import * as hmUI from '@zos/ui';
-import { Screen, Time } from '@zos/sensor'
+import { Time } from '@zos/sensor'
 
 export function buildView() {
 
-
-    console.log('**** BUILD VIEW 2***');
-
-    const screen = new Screen();
     const time = new Time();
-
 
     hmUI.createWidget(hmUI.widget.IMG, {
         x: px(0),
@@ -25,39 +20,6 @@ export function buildView() {
         w: px(480),
         h: px(480),
         src: 'bg-aod.png',
-        show_level: hmUI.show_level.ONLY_AOD,
-    });
-
-    hmUI.createWidget(hmUI.widget.TIME_POINTER, {
-        hour_centerX: 240,
-        hour_centerY: 240,
-        hour_posX: 16,
-        hour_posY: 139,
-        hour_path: 'hand-hour.png',
-        minute_centerX: 240,
-        minute_centerY: 240,
-        minute_posX: 9,
-        minute_posY: 189,
-        minute_path: 'hand-minute.png',
-        second_centerX: 240,
-        second_centerY: 240,
-        second_posX: 10,
-        second_posY: 190,
-        second_path: 'hand-second.png',
-        show_level: hmUI.show_level.ONLY_NORMAL,
-    });
-
-    hmUI.createWidget(hmUI.widget.TIME_POINTER, {
-        hour_centerX: 240,
-        hour_centerY: 240,
-        hour_posX: 16,
-        hour_posY: 139,
-        hour_path: 'hand-hour-aod.png',
-        minute_centerX: 240,
-        minute_centerY: 240,
-        minute_posX: 9,
-        minute_posY: 189,
-        minute_path: 'hand-minute-aod.png',
         show_level: hmUI.show_level.ONLY_AOD,
     });
 
@@ -106,39 +68,60 @@ export function buildView() {
         angle: angle_dow,
         show_level: hmUI.show_level.ONLY_NORMAL,
     });
+
+    hmUI.createWidget(hmUI.widget.TIME_POINTER, {
+        hour_centerX: 240,
+        hour_centerY: 240,
+        hour_posX: 16,
+        hour_posY: 139,
+        hour_path: 'hand-hour.png',
+        minute_centerX: 240,
+        minute_centerY: 240,
+        minute_posX: 9,
+        minute_posY: 189,
+        minute_path: 'hand-minute.png',
+        second_centerX: 240,
+        second_centerY: 240,
+        second_posX: 10,
+        second_posY: 190,
+        second_path: 'hand-second.png',
+        show_level: hmUI.show_level.ONLY_NORMAL,
+    });
+
+    hmUI.createWidget(hmUI.widget.TIME_POINTER, {
+        hour_centerX: 240,
+        hour_centerY: 240,
+        hour_posX: 16,
+        hour_posY: 139,
+        hour_path: 'hand-hour-aod.png',
+        minute_centerX: 240,
+        minute_centerY: 240,
+        minute_posX: 9,
+        minute_posY: 189,
+        minute_path: 'hand-minute-aod.png',
+        show_level: hmUI.show_level.ONLY_AOD,
+    });
+    
+    /* ORIGINALLY WAS DOING THIS EVERY MIN AS NOT SURE WHAT getHours
+       WILL GET ME WITH THE onPerhHourEnd callback - going to try
+       THAT SINCE IT WOULD USE LESS PROCESSING (1/hr vs 1/min)
+       BUT MAY NEED TO FALLBACK TO THIS IF THAT DOESNT WORK
     
     time.onPerMinute(function() {
+        const angle = time.getHours() * 15;
+        subdial_24_widget.setProperty(hmUI.prop.ANGLE, angle);
+    });
+    */
+
+    time.onPerHourEnd(function() {
         const angle = time.getHours() * 15;
         subdial_24_widget.setProperty(hmUI.prop.ANGLE, angle);
     });
 
     time.onPerDay(function() {
         const angle_day = time.getDate() * 11.613;
-        subdial_day_widget.setProperty(hmUI.prop.ANGLE, angle);
+        const angle_dow = time.getDay() * 51.429;
+        subdial_day_widget.setProperty(hmUI.prop.ANGLE, angle_day);
+        subdial_dow_widget.setProperty(hmUI.prop.ANGLE, angle_dow);
     });
-
-    /*
-    screen.onChange(function() {
-        const current_time = time.getTime();
-        console.log('screen status: ' + screen.getStatus());
-        console.log('hours: ' + time.getHours());
-        console.log('minutes: ' + time.getMinutes());
-        console.log('seconds' + time.getSeconds());
-        console.log('utc timestamp: ' + current_time);
-    });
-
-    time.onPerMinute(function() {
-        const current_time = time.getTime();
-        console.log('*** MINUTE CALLABCK');
-        console.log('hours: ' + time.getHours());
-        console.log('minutes: ' + time.getMinutes());
-        console.log('seconds' + time.getSeconds());
-        console.log('utc timestamp: ' + current_time);
- 
-    });
-    */
-
-
-//    let subdial_24_params = getSubdial24PointerParams();
-  //  subdial_24_pointer.setProperty(hmUI.prop.MORE, wind_force_params);
 }
